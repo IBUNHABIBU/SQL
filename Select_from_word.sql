09bb8ff28b22523d804193af3509c91d16f29812
IBUNHABIBU <ibunhabib@gmail.com>
2019-10-13 13:09:07 +0300

    select from word

---
+++ Select_from_word.sql
@@ -0,0 +1,39 @@
+-- Question 1
+SELECT name, continent, population FROM world
+
+-- Question 2
+SELECT name FROM world
+WHERE population > 200000000
+
+-- Question 3
+SELECT name, GDP/population FROM world
+WHERE population >200000000
+
+-- Question 4
+SELECT name, population/1000000 FROM world
+WHERE continent = 'South America'
+
+-- Question 5
+SELECT name, population FROM world
+WHERE name IN('France','Germany','Italy');
+
+-- Question 6
+SELECT name FROM world
+WHERE name LIKE '%United%'
+
+-- Question 7
+SELECT name,population,area FROM world
+WHERE area> 3000000 OR population>250000000
+
+-- Question 8
+SELECT name,population,area FROM world
+WHERE area> 3000000 XOR population>250000000
+
+-- Question 9
+SELECT name,ROUND(population/1000000,2) ,ROUND(GDP/1000000000,2) FROM world
+WHERE continent ='South America'
+
+-- Question 10
+SELECT name, ROUND(GDP/population,-3) AS per_capita_GDP FROM world
+WHERE GDP>1000000000000
+
